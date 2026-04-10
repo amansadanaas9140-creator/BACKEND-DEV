5b1d2b32d75814c1febf72b7130c9ebc48345da2
Aman Sadana <amansadana.as9140@gmail.com>
2026-04-10 14:28:06 +0530

    [Aman Sadana].Authentication_Authorization.addedfile Book.js

---
+++ library management system/models/Book.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  bookId: { type: Number, required: true, unique: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  price: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
