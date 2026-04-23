40c7f1468775f5fc1ba0be204fe3ca5efa524748
Aman Sadana <amansadana.as9140@gmail.com>
2026-04-23 13:50:32 +0530

    Delete library management system directory

--- library management system/models/Member.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const memberSchema = new mongoose.Schema({
-  memberId: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  membershipType: { type: String, required: true, enum: ['Normal', 'Gold'] }
-});
-
-module.exports = mongoose.model('Member', memberSchema);
