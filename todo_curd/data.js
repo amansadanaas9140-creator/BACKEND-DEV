806948f4cdb7bd6ee873aacf4ea9626560097a90
Aman Sadana <amansadana.as9140@gmail.com>
2026-04-23 15:23:46 +0530

    Delete todo_curd directory

--- todo_curd/data.js
+++
@@ -1,8 +0,0 @@
-// Array to store our todos and state management functions
-let todos = [];
-
-module.exports = {
-    getTodos: () => todos,
-    setTodos: (newTodos) => { todos = newTodos; },
-    addTodoToState: (todo) => { todos.push(todo); }
-};
