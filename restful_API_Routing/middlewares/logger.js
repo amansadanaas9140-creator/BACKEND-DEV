b60c543e9ec320478ae9225f57283a343d5a11d4
Aman Sadana <amansadana.as9140@gmail.com>
2026-04-23 13:50:45 +0530

    Delete restful_API_Routing directory

--- restful_API_Routing/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-// Logs the HTTP method, the URL, and the timestamp of every request
-const logger = (req, res, next) => {
-  const now = new Date().toISOString();
-  console.log(`[${now}] ${req.method} request to ${req.originalUrl}`);
-  next();
-};
-
-module.exports = logger;
