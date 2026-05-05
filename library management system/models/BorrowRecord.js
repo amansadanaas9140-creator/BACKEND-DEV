c6178273d283fcbb7e01e52f3fceb7687447eb49
Aman Sadana <amansadana.as9140@gmail.com>
2026-05-05 15:33:42 +0530

    Delete library management system directory

--- library management system/models/BorrowRecord.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const borrowRecordSchema = new mongoose.Schema({
-  member: { type: mongoose.Schema.Types.ObjectId, ref: 'Member', required: true },
-  books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
-  dateBorrowed: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('BorrowRecord', borrowRecordSchema);
