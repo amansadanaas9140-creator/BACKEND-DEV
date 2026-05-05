07a033a5e1ca5302444e5424d48070f850df2ad4
Aman Sadana <amansadana.as9140@gmail.com>
2026-05-05 15:34:03 +0530

    Delete rest-security directory

--- rest-security/quickBank/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { login, forgotPassword } = require('../controllers/authController');
-
-const router = express.Router();
-
-router.post('/login', login);
-router.post('/forgotpassword', forgotPassword);
-
-module.exports = router;
